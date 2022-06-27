7d83a4988e78ac1279d7df90a2c02f4575898a1c
kai <kai@kaivong.com>
2022-06-27 05:09:16 +0100

    edit script

--- interests.rb
+++ interests.rb
@@ -9,12 +9,17 @@ interests = all_interests.map { |interest_keyword|  interests_by_alphabet[intere
 list_of_interests = String.new
 HTML_ORDERED_LIST = 'ol'
 HTML_LIST_ITEM = 'li'
+
+def wrap_in_html_BoilerPlate(html)
+	"<html><head></head><body>"+ html + "</body></html>"
+end
+
 interests_by_alphabet.map do |key, k_interests|
 	h2_attrs = {'html_element' => html_header2, 'attr_id' => key.to_s}
 	list_of_interests += HyperTextFromMarkdownParser.new(key.to_s, h2_attrs).results
 	k_interests_html = k_interests.map { |interest| url_link = "#{SEARCH_URL}?q=#{interest.gsub(' ','%20')}"; interest_link_md = "[#{interest}](#{url_link} 'external')"; HyperTextFromMarkdownParser.new(interest_link_md, HTML_LIST_ITEM).results }.join
 	list_of_interests += k_interests_html
 end
-html_text = "<html><head></head><body>"+ HyperTextFromMarkdownParser.new(list_of_interests, HTML_ORDERED_LIST).results + "</body></html>"
+html_text = wrap_in_html_BoilerPlate(HyperTextFromMarkdownParser.new(list_of_interests, HTML_ORDERED_LIST).results)
 file_of_interests << html_text
 file_of_interests.close
